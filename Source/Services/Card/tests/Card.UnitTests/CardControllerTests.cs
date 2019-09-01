58b8e841c9e7cab629638227afdcd47894c1dfeb
Enes Dalga <enesdalgaa@gmail.com>
2019-09-01 11:05:25 +0300

    Unit test solution added

---
+++ Source/Services/Card/tests/Card.UnitTests/CardControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Card.API.Controllers;
+using Card.Domain.Models;
+using Card.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Card.UnitTests
+{
+    public class CartControllerTests
+    {
+        
+    }
+
+}
